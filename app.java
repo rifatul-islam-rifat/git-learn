1f73cc79c301b1c5729544751f163ac5d0d7744e
rifat <islammdrifatul776@gmail.com>
2025-06-29 18:21:34 +0600

    fourth commit

--- app.java
+++ app.java
@@ -5,7 +5,11 @@ public class app{
         int result = a+b;
         int sum = 0;
         sum += sum + result;
+        int mul = 0;
+        mul -= mul - result;
         System.out.println("The result is : " + result);
         System.out.println("The sum is : " + sum);
+        System.out.println("The mul is : " + mul);
+
     }
 }
