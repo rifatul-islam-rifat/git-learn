d3d258f439226ca3fb4c83a62c4d9659875f43f3
rifat <islammdrifatul776@gmail.com>
2025-06-29 18:11:11 +0600

    third commit

--- app.java
+++ app.java
@@ -3,6 +3,9 @@ public class app{
         int a = 10;
         int b = 20;
         int result = a+b;
+        int sum = 0;
+        sum += sum + result;
         System.out.println("The result is : " + result);
+        System.out.println("The sum is : " + sum);
     }
 }
